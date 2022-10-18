c6097e97646412141666e1c8c7876927c0308069
Yannick Baron <yannick.baron@thinktecture.com>
2022-10-18 11:30:00 +0200

    feat(demo): add name parameter

--- demo.ts
+++ demo.ts
@@ -1,4 +1,6 @@
 export interface MyGreatInterface {
+  name: string;
+
   x: number;
   y: number;
 }
@@ -7,11 +9,11 @@ function dummyFn({ x, y }: MyGreatInterface): number {
   return x * 2 + y;
 }
 
-const o1: MyGreatInterface = { x: 5, y: 10 };
-const o2: MyGreatInterface = { x: 1, y: 2 };
+const o1: MyGreatInterface = { x: 5, y: 10, name: "O1" };
+const o2: MyGreatInterface = { x: 1, y: 2, name: "O2" };
 
 function logIt(o: MyGreatInterface): void {
-  console.log(dummyFn(o));
+  console.log(o.name, dummyFn(o));
 }
 
 logIt(o1);
