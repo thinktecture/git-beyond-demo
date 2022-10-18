34d4463f45cdca3c9c35b8c47c0f9c8b736076f3
Yannick Baron <yannick.baron@thinktecture.com>
2022-10-18 11:30:00 +0200

    feat(demo): add name parameter and move interface

--- demo.ts
+++ demo.ts
@@ -1,17 +1,19 @@
-export interface MyGreatInterface {
-  x: number;
-  y: number;
-}
-
 function dummyFn({ x, y }: MyGreatInterface): number {
   return x * 2 + y;
 }
 
-const o1: MyGreatInterface = { x: 5, y: 10 };
-const o2: MyGreatInterface = { x: 1, y: 2 };
+export interface MyGreatInterface {
+  name: string;
+
+  x: number;
+  y: number;
+}
+
+const o1: MyGreatInterface = { x: 5, y: 10, name: "O1" };
+const o2: MyGreatInterface = { x: 1, y: 2, name: "O2" };
 
 function logIt(o: MyGreatInterface): void {
-  console.log(dummyFn(o));
+  console.log(o.name, dummyFn(o));
 }
 
 logIt(o1);
