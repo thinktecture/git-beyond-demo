f51b40dd7011ddaaacd43b7625b3d5007ee03a20
Yannick Baron <yannick.baron@thinktecture.com>
2022-10-18 11:25:51 +0200

    feat(demo): add more stuff

--- demo.ts
+++ demo.ts
@@ -1,3 +1,18 @@
-function dummyFn(x: number): number {
-  return x * 2;
+export interface MyGreatInterface {
+  x: number;
+  y: number;
 }
+
+function dummyFn({ x, y }: MyGreatInterface): number {
+  return x * 2 + y;
+}
+
+const o1: MyGreatInterface = { x: 5, y: 10 };
+const o2: MyGreatInterface = { x: 1, y: 2 };
+
+function logIt(o: MyGreatInterface): void {
+  console.log(dummyFn(o));
+}
+
+logIt(o1);
+logIt(o2);
